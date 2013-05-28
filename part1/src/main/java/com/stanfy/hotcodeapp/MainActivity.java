07d66a50251d59a822244435e8f9eaece3db5813
Roman Mazur <mazur.roman@gmail.com>
2013-05-28 09:45:53 +0300

    test

--- part1/src/main/java/com/stanfy/hotcodeapp/MainActivity.java
+++ part1/src/main/java/com/stanfy/hotcodeapp/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends Activity {
 
   @Override
   protected void onCreate(final Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
+       super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
     final View mainPanel = findViewById(R.id.main_panel);
