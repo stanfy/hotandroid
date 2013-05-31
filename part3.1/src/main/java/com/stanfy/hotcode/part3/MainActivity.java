4973810c51842e9af7cab0c6c137e779a2349d2b
Stanislav Chumarin <stanislav.chumarin@gmail.com>
2013-05-31 17:47:42 +0300

    part3.1 loaders and providers

--- part3.1/src/main/java/com/stanfy/hotcode/part3/MainActivity.java
+++ part3.1/src/main/java/com/stanfy/hotcode/part3/MainActivity.java
@@ -11,41 +11,42 @@ import android.support.v4.widget.CursorAdapter;
  */
 public class MainActivity extends FragmentActivity {
 
-  /** Service action. */
-  public static final String SERVICE_ACTION = "edu.hotcode.write";
+    /**
+     * Service action.
+     */
+    public static final String SERVICE_ACTION = "edu.hotcode.write";
 
-  @Override
-  protected void onCreate(final Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
-  }
-
-  public CursorAdapter getListAdapter() {
-    final ListFragment f = (ListFragment) getSupportFragmentManager().findFragmentById(R.id.list_fragment);
-    return f == null ? null : f.getListAdapter();
-  }
-
-  public String getPersonNameColumnName() {
-    //FIXME
-    return null;
-  }
-  public String getPersonScoreColumnName() {
-    //FIXME
-    return null;
-  }
-
-  public void showDetails(final Person person) {
-    final FragmentManager fm = getSupportFragmentManager();
-    final DetailsFragment f = new DetailsFragment();
-    final Bundle args = new Bundle();
-    args.putSerializable(DetailsFragment.ARG_PERSON, person);
-    f.setArguments(args);
-    if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
-      fm.popBackStack();
-      fm.beginTransaction().replace(R.id.fragment_container, f).addToBackStack(null).commit();
-    } else {
-      fm.beginTransaction().hide(fm.findFragmentById(R.id.list_fragment)).replace(R.id.fragment_container, f).addToBackStack(null).commit();
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
     }
-  }
+
+    public CursorAdapter getListAdapter() {
+        final ListFragment f = (ListFragment) getSupportFragmentManager().findFragmentById(R.id.list_fragment);
+        return f == null ? null : f.getListAdapter();
+    }
+
+    public String getPersonNameColumnName() {
+        return Person.Contract.COLUMN_NAME;
+    }
+
+    public String getPersonScoreColumnName() {
+        return Person.Contract.COLUMN_SCORE;
+    }
+
+    public void showDetails(final Person person) {
+        final FragmentManager fm = getSupportFragmentManager();
+        final DetailsFragment f = new DetailsFragment();
+        final Bundle args = new Bundle();
+        args.putSerializable(DetailsFragment.ARG_PERSON, person);
+        f.setArguments(args);
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            fm.popBackStack();
+            fm.beginTransaction().replace(R.id.fragment_container, f).addToBackStack(null).commit();
+        } else {
+            fm.beginTransaction().hide(fm.findFragmentById(R.id.list_fragment)).replace(R.id.fragment_container, f).addToBackStack(null).commit();
+        }
+    }
 
 }
