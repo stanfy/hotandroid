04e4248eaa5a46bf2d3ddd034ee6643df9c20bab
Stanislav Chumarin <stanislav.chumarin@gmail.com>
2013-05-31 14:34:03 +0300

    part1 color rotation fix

--- part1/src/main/java/com/stanfy/hotcode/part1/MainActivity.java
+++ part1/src/main/java/com/stanfy/hotcode/part1/MainActivity.java
@@ -11,64 +11,75 @@ import android.view.View.OnClickListener;
  * Main screen with three "color" button and "start service" button.
  */
 public class MainActivity extends Activity {
-  public static final String ACTION_START_EXAMPLE = "com.example.hotcode.START_EXAMPLE";
+    public static final String ACTION_START_EXAMPLE = "com.example.hotcode.START_EXAMPLE";
 
-  private static final String STATE_BG_COLOR = "bg_color";
+    private static final String STATE_BG_COLOR = "bg_color";
 
-  private int currentColor;
+    private int currentColor;
+    private View mMainPanel;
 
-  /**
-   * The only method that must be override. You have to define activity layout in this method.
-   * @param savedInstanceState values from "previous" activity. This parameter can be null (e.g. activity is started for
-   *                           the first time)
-   */
-  @Override
-  protected void onCreate(final Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+    /**
+     * The only method that must be override. You have to define activity layout in this method.
+     *
+     * @param savedInstanceState values from "previous" activity. This parameter can be null (e.g. activity is started for
+     *                           the first time)
+     */
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-    final View mainPanel = findViewById(R.id.main_panel);
+        mMainPanel = findViewById(R.id.main_panel);
 
-    if (savedInstanceState != null && savedInstanceState.containsKey(STATE_BG_COLOR)) {
-      currentColor = savedInstanceState.getInt(STATE_BG_COLOR);
+        if (savedInstanceState != null && savedInstanceState.containsKey(STATE_BG_COLOR)) {
 
-      // FIXME: there you should restore background color
+            setCurrentColor(savedInstanceState.getInt(STATE_BG_COLOR));
+        }
+
+        findViewById(R.id.red_button).setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                setCurrentColor(Color.RED);
+            }
+        });
+
+        findViewById(R.id.green_button).setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                setCurrentColor(Color.GREEN);
+            }
+        });
+
+        findViewById(R.id.blue_button).setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                setCurrentColor(Color.BLUE);
+            }
+        });
+
+        findViewById(R.id.start_service_button).setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                startService(new Intent(MainActivity.this, ExampleService.class));
+            }
+        });
     }
 
-    findViewById(R.id.red_button).setOnClickListener(new OnClickListener() {
-      @Override
-      public void onClick(final View v) {
-        mainPanel.setBackgroundColor(Color.BLACK);
-        currentColor = 0;
-      }
-    });
+    private void setCurrentColor(int currentColor) {
+        this.currentColor = currentColor;
+        mMainPanel.setBackgroundColor(currentColor);
+    }
 
-    findViewById(R.id.green_button).setOnClickListener(new OnClickListener() {
-      @Override
-      public void onClick(final View v) {
-        // FIXME: oops, something is missed here
-      }
-    });
+    /**
+     * In this method you can save your current state. It will be called before activity destruction.
+     *
+     * @param outState states from parent classes
+     */
+    @Override
+    protected void onSaveInstanceState(final Bundle outState) {
+        super.onSaveInstanceState(outState);
 
-    // FIXME: don't forget about blue button...
-
-    findViewById(R.id.start_service_button).setOnClickListener(new OnClickListener() {
-      @Override
-      public void onClick(final View v) {
-        startService(new Intent(MainActivity.this, ExampleService.class));
-      }
-    });
-  }
-
-  /**
-   * In this method you can save your current state. It will be called before activity destruction.
-   * @param outState states from parent classes
-   */
-  @Override
-  protected void onSaveInstanceState(final Bundle outState) {
-    super.onSaveInstanceState(outState);
-
-    outState.putInt(STATE_BG_COLOR, currentColor);
-  }
+        outState.putInt(STATE_BG_COLOR, currentColor);
+    }
 
 }
