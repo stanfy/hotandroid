1c2c8cd3124c5fb890e03b2b756055f0edd87299
Stanislav Chumarin <stanislav.chumarin@gmail.com>
2013-05-31 12:31:29 +0300

    QuickStart

--- part0/src/main/java/com/stanfy/hotcode/part0/MainActivity.java
+++ part0/src/main/java/com/stanfy/hotcode/part0/MainActivity.java
@@ -5,11 +5,11 @@ import android.os.Bundle;
 
 public class MainActivity extends Activity {
 
-  @Override
-  protected void onCreate(final Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    // FIXME: setContentView is the main method for providing layout or View for Activity
-    //setContentView(R.layout.activity_main);
-  }
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // FIXME: setContentView is the main method for providing layout or View for Activity
+        setContentView(R.layout.activity_main);
+    }
 
 }
